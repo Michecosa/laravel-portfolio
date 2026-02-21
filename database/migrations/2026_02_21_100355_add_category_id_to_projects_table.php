eafcfb6691200154332eed9f05946eb11c9f8e2e
Michela <micheladellagatta1@gmail.com>
2026-02-21 11:16:36 +0100

    Add Category model, migration, seeder, and update DatabaseSeeder to include categories

---
+++ database/migrations/2026_02_21_100355_add_category_id_to_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            // Aggiungo la colonna e la costrain
+            $table->foreignId('category_id')->default(1)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+};
