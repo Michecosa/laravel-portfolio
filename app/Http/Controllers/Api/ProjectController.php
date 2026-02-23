7edb1496e6f984a0fc7043c4b3a925f9ffba77b5
Michela <micheladellagatta1@gmail.com>
2026-02-23 11:56:55 +0100

    Implement index method in ProjectController to return API response

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index() {
+        return "Sei nella index API";
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/* 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); 
+*/
+
+# http://127.0.0.1:8000/api/projects
+Route::get('projects', [ProjectController::class, 'index']);
